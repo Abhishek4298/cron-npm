704107d3c6357c3ed507a87802447f220a72e84c
Abhishek4298 <abhishek.bhavsar@bacancytechnology.com>
2020-09-18 13:58:09 +0530

    adding multiple CRON job

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express();
 const CronJob = require('cron').CronJob;
 
 app.get('/', function (req, res) {
-	new CronJob('* * * * * *', function () {
-		console.log("staring cron");
-	}, null, true, 'America/Los_Angeles');
+	console.log('Before job instantiation');
+	const job = new CronJob('*/2 * * * * *', function () {
+		const d = new Date();
+		console.log('First get hour-minute :', d.getHours(), d.getMinutes());
+	});
+
+	const job2 = new CronJob('*/5 * * * * *', function () {
+		const d = new Date();
+		console.log('Second get date:', d.getDate());
+	});
+	console.log('After job instantiation');
+	job.start();
+	job2.start();
 });
 
 app.listen(3000, () => {
